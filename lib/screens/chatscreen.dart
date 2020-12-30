9229a3984805fc9b43ae10711cddf89afb0d5466
playerash <alysonvalente@hotmail.com>
2020-12-30 12:14:28 -0300

    envolvendo a mensagem do chat no container

--- lib/screens/chatscreen.dart
+++ lib/screens/chatscreen.dart
@@ -57,46 +57,60 @@ class _ChatScreenState extends State<ChatScreen> {
                   image: AssetImage("images/backgroundwpp.png"),
                   fit: BoxFit.fill)),
         ),
-        Row(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 40,
-                width: 285,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: Colors.white),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.emoji_emotions_outlined,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                            labelText: 'Digite uma mensagem',
-                            border: InputBorder.none),
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
             Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xFF075E54),
-              ),
-              child: Icon(Icons.mic, color: Colors.white,),
-            )
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Color(0xffdffec5)),
+              child: Text(widget._contato.mensagem),
+                
+            ),
+            Row(
+              children: [
+                
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 285,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.white),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.emoji_emotions_outlined,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Digite uma mensagem',
+                                border: InputBorder.none),
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF075E54),
+                  ),
+                  child: Icon(Icons.mic, color: Colors.white,),
+                )
+              ],
+            ),
           ],
         ),
       ]),
