b3eb5910ed0210e9faa74636127384502aefb2f1
playerash <alysonvalente@hotmail.com>
2020-12-31 15:51:20 -0300

    Ajuste na inicialização da camera

--- lib/screens/camerascreen.dart
+++ lib/screens/camerascreen.dart
@@ -17,9 +17,11 @@ class _CameraScreenState extends State<CameraScreen> {
     //escolhendo qual camera usar e inicializando a camera
     _cameraController =
         CameraController(widget.cameras[0], ResolutionPreset.medium);
-    _cameraController.initialize();
+    _cameraController.initialize().then((_) {
+      setState(() {});
+    });
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -30,61 +32,64 @@ class _CameraScreenState extends State<CameraScreen> {
       );
     }
   //Camera carregada
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Container(
-          width: double.infinity,
-          child: AspectRatio(
-            aspectRatio: _cameraController.value.aspectRatio,
-            // Faz com que a camera seja visualizada na tela
-            child: CameraPreview(
-              _cameraController,
+    return Material(
+          child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: double.infinity,
+            height: size.height,
+            child: AspectRatio(
+              aspectRatio: _cameraController.value.aspectRatio,
+              // Faz com que a camera seja visualizada na tela
+              child: CameraPreview(
+                _cameraController,
+              ),
             ),
           ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                      icon: Icon(
-                        Icons.flash_off,
-                        color: Colors.white,
-                        size: 25,
-                      ),
-                      onPressed: () {}),
-                  Container(
-                    height: size.width / 5,
-                    width: size.height / 5,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white, width: 2),
-                        shape: BoxShape.circle),
-                  ),
-                  IconButton(
-                      icon: Icon(
-                        Icons.switch_camera,
-                        color: Colors.white,
-                        size: 25,
-                      ),
-                      onPressed: () {})
-                ],
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.flash_off,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                        onPressed: () {}),
+                    Container(
+                      height: size.width / 5,
+                      width: size.height / 5,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 2),
+                          shape: BoxShape.circle),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.switch_camera,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                        onPressed: () {})
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Segure para vídeo, toque para foto",
-                style: TextStyle(color: Colors.white),
-              ),
-            )
-          ],
-        )
-      ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Segure para vídeo, toque para foto",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
